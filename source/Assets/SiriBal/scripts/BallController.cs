1a866ff3d5999f5b9d9a660e18b96386120b8fa9
yamamotoshuhei888 <yamamoto.shuhei888@gmail.com>
2019-09-20 09:08:01 +0900

    change directry ball prefab

---
+++ source/Assets/SiriBal/scripts/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class BallContoroller : MonoBehaviour
+{
+    float timeOut=3.0f;
+    float timeElapsed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if(timeElapsed >= timeOut) {
+            Destroy(gameObject);
+            //timeElapsed = 0.0f;
+        }
+    }
+}
