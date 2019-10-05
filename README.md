5ce29a3ee94fd15a5e86f14fb02a11f48ff746ed
sgmtsnj <sgmt.snj@gmail.com>
2019-10-06 01:08:02 +0900

    #18  ログイン画面作った（いっぱいコミットしてごめんね）

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ Serious Balloon
 buildフォルダをビルド成果物の保存先にすると、gitコミット対象からは外してくれるようにしています。
 buildフォルダを個人で作ってね
 
+## Firebaseのビルド
+
+このアプリケーションでは User Authentication のために、 Google Firebase Authentication を使っています。
+Firebase を使用したアプリケーションをビルドする場合、以下の点に注意してください。
+
+- APIKeyを適切に配置する（コミットしない）
+    Google Firebase は BaaS (Backend as a Service) です。このような Web サービスには接続用 API Key が必要です。しかし、 API Key をむやみに公開すると、その API Key を使って大量のアクセスを受けた際に、所有者のアカウントに対して多額の費用が請求されることがあります。そのため、 API Key を含むファイルは厳重に管理され、 GitHub のような Public アクセス可能なところにおいてはいけません。API Key の管理者から適切に譲り受け、正しく取り扱いましょう。
+
+
+- Cocoapodsをインストールする
+    Firebase は Xcode ビルド時にのパッケージマネージャーとして Cocoapods を使用しています。事前にインストールしてください。Macの場合、はじめから Ruby はインストールされていますので、いきなり以下のコマンドから初めて大丈夫です。心配な方は gem が入っていることを確認してください。 `gem --version` で確認可能です。
+    ```shell
+    $$ sudo gem install -n usr/local/bin cocoapods
+    ```
+    これで Cocoapods のインストールが完了します。続いて、
+    ```shell
+    $$ pod setup
+    ```
+    として、 Cocoapods の環境をセットアップしてください。これにて完了です。
--- source/Assets/SiriBal/scripts/LoginButtonController.cs
+++ source/Assets/SiriBal/scripts/LoginButtonController.cs
@@ -2,23 +2,175 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoginButtonController : MonoBehaviour
 {
+    // Define
+    private enum GameScene{
+        None = 0,
+        Main,
+        Login,
+        Score,
+        Thankyou,
+    }
+
+    // Common values
+    private string _mainMessage = "";
+    private bool _isMessageChanged = false;
+    private GameScene _currentScene = GameScene.Login;
+    private bool _isSceneChanged = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(_isSceneChanged)
+        {
+            ChangeScene(_currentScene);
+            _isSceneChanged = false;
+        }
         
+        if (!System.String.IsNullOrEmpty(_mainMessage))
+        {
+            ShowMessageOnScreen(_mainMessage);
+            _isMessageChanged = false;
+        }
     }
 
     public void LoginToSiribal()
     {
-        SceneManager.LoadScene("SeriousBalloon");
+        // Firebase Authentication Static Instance
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.SignOut();
+        
+        // Email & Password from input field
+        var email = GameObject.Find("EmailInputField/Text").GetComponent<Text>().text;
+        var password = GameObject.Find("PasswordInputField/Text").GetComponent<Text>().text;
+
+        if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+        {
+            _mainMessage = "Invalid email or password. Please check your email or password again.";
+            _isMessageChanged = true;
+            return;
+        }
+
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if(task.Exception == null){
+                if (task.IsCanceled) {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted) {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                    return;
+                }
+                Firebase.Auth.FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+
+                if (newUser.IsEmailVerified)
+                {
+                    _mainMessage = "success";
+                    _isMessageChanged = true;
+                    _currentScene = GameScene.Main;
+                    _isSceneChanged = true;
+                }
+                else
+                {
+                    newUser.SendEmailVerificationAsync();
+                    _mainMessage = "This emali has not verified yet. We've sent you email again. Please verify from the link.";
+                    _isMessageChanged = true;
+                }
+            }
+            else{
+                foreach(var innerExc in task.Exception.InnerExceptions)
+                {
+                    _mainMessage = innerExc.InnerException.Message;
+                }
+                _isMessageChanged = true;
+            }
+        });
+    }
+
+    public void CreateNewUser()
+    {
+        // Email & Password from input field
+        var email = GameObject.Find("EmailInputField/Text").GetComponent<Text>().text;
+        var password = GameObject.Find("PasswordInputField/Text").GetComponent<Text>().text;
+
+        // Firebase Authentication Static Instance
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password)){
+            _mainMessage = "Invalid email or password. Please check your email or password again.";
+            _isMessageChanged = true;
+            return;
+        }
+
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(
+            task => {
+                if (task.Exception == null)
+                {
+                    if (task.IsCanceled) {
+                        Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                        return;
+                    }
+                    if (task.IsFaulted) {
+                        Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                        return;
+                    }
+                    // Firebase user has been created.
+                    Firebase.Auth.FirebaseUser newUser = task.Result;
+                    Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                        newUser.DisplayName, newUser.UserId);
+                    newUser.SendEmailVerificationAsync();
+                    
+                    _mainMessage = "success";
+                    _isMessageChanged = true;
+                    _currentScene = GameScene.Thankyou;
+                    _isSceneChanged = true;
+                }
+                else
+                {
+                    foreach(var innerExc in task.Exception.InnerExceptions)
+                    {
+                        _mainMessage = innerExc.InnerException.Message;
+                    }
+                    _isMessageChanged = true;
+                }
+            }
+        );
+    }
+    void ChangeScene(GameScene eGameScene)
+    {    
+        switch(eGameScene)
+        {
+            case GameScene.Login:
+                SceneManager.LoadScene("LoginForm");
+                break;
+            case GameScene.Main:
+                SceneManager.LoadScene("SeriousBalloon");
+                break;
+            case GameScene.Thankyou:
+                SceneManager.LoadScene("ThankYou");
+                break;
+            case GameScene.Score:
+            case GameScene.None:
+            default:
+                Debug.Assert(false, "NotImpl");
+                break;
+        }
+    }
+
+    void ShowMessageOnScreen(string message)
+    {
+        var mesObject = GameObject.Find("Message").GetComponent<Text>();
+        mesObject.text = message;
     }
 }
+
