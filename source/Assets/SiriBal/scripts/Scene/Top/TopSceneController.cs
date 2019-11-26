2333c500ac31697b8acdd8d400b1021d788d33b0
sgmtsnj <sgmt.snj@gmail.com>
2019-11-26 23:53:02 +0900

    トップ画面変更 #26 と アイコン仮設定 #27

---
+++ source/Assets/SiriBal/scripts/Scene/Top/TopSceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Generic;
+using Generic.Manager;
+
+public class TopSceneController : MonoBehaviour
+{
+    public void Start()
+    {
+
+    }
+    public void Update()
+    {
+
+    }
+    public void PlayButtonClicked()
+    {
+        GameSceneManager.ChangeScene(GameScenes.SeriousBalloon);
+    }
+
+    public void LankingButtonClicked()
+    {
+        GameSceneManager.ChangeScene(GameScenes.ScoreBoard);
+    }
+    
+    public void OptionsButtonClicked(){
+        // Not impl
+    }
+}
