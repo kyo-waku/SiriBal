265f5db3a88f7efbce63117929993b7abfe49e6d
5104 <satoutakuyasatoutakuya@gmail.com>
2019-12-16 00:58:34 +0900

    バルーンを全部置いたら、シーン切替用のLoadingバルーン生成するようにしたよ。

---
+++ source/Assets/SiriBal/scripts/Scene/ARGame/LoadingBalloonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingBalloonController : MonoBehaviour
+{
+    Vector3 LoadingBalloonInitForce = new Vector3 (0, 50000,0);
+
+// Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(LoadingBalloonInitForce);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ source/Assets/SiriBal/scripts/Scene/ARGame/LoadingBalloonGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingBalloonGenerator : MonoBehaviour
+{
+    private GameDirector GameDirector;
+    private GameModeController gameMode;
+    public GameObject LoadingBalloonPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameDirector = GameObject.Find("GameDirector").GetComponent<GameDirector>();
+        gameMode = GameObject.Find("ModeSwitcher").GetComponent<GameModeController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameMode.GameMode == GameModeController.eGameMode.Balloon || gameMode.GameMode == GameModeController.eGameMode.WaitTime)
+        {
+            //GameDirector.BalloonLimitはPublicにして良いか確認してから反映
+            //if (GameDirector.BalloonCounter >= GameDirector.BalloonLimit || GameDirector.TimeValue <= 0 || Input.GetKeyDown(KeyCode.L))
+            //Lキーはdebug用。
+            if (GameDirector.BalloonCounter >= 30 || GameDirector.TimeValue <= 0 || Input.GetKeyDown(KeyCode.L))
+            {
+                GameDirector.BalloonCounter = 0;
+                gameMode.GameMode = GameModeController.eGameMode.WaitTime;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        GameObject LoadingBalloon = Instantiate(LoadingBalloonPrefab) as GameObject;
+                        //UIのImageはCanvasの子じゃないとGUI表示されないため
+                        var canvas = GameObject.FindObjectOfType<Canvas>();
+                        LoadingBalloon.transform.SetParent(canvas.transform, false);
+
+                        LoadingBalloon.transform.position = new Vector3(j * 150, (i * 150) - 2000, 0);
+                    }
+                }
+            }
+        }
+    }
+}
