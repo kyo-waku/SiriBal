dd2ebcafb954e5c337faa40a479686d1be11f63d
yamamotoshuhei888 <yamamoto.shuhei888@gmail.com>
2020-03-13 09:12:02 +0900

    ぶつかると音が出るよ。PlayBGMの音70％にしたよ。ハンマーの投げれる距離を増加したよ。

---
+++ source/Assets/SiriBal/scripts/Common/Tools/HitPlaySound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour {
+
+    public AudioClip sound;
+    private GameObject MainCamera;
+
+    void OnCollisionEnter(Collision collision) {
+        //メインカメラの位置を見つける
+        Vector3 tmp = GameObject.Find("MainCamera").transform.position;
+        AudioSource.PlayClipAtPoint(sound, tmp);
+        //Debug.Log("SE-played");
+    }
+}
