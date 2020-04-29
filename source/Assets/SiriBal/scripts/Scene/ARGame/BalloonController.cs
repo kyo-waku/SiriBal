ab543eb729cfe90ccd27eb47129b4984de606f19
sgmtsnj <sgmt.snj@gmail.com>
2020-04-29 23:24:53 +0900

    #68 自分の近くにはできないよう微妙にマージンを設けた

--- source/Assets/SiriBal/scripts/Scene/ARGame/BalloonController.cs
+++ source/Assets/SiriBal/scripts/Scene/ARGame/BalloonController.cs
@@ -141,25 +141,56 @@ public class BalloonController : MonoBehaviour {
 				float RandomPositionX = Random.Range(-15,15)/10.0f;
 				float RandomPositionY = Random.Range(-30,20)/10.0f;
 				float RandomPositionZ = Random.Range(0,5);
-
-				Vector3 RandomPosition = new Vector3(RandomPositionX, RandomPositionY, RandomPositionZ);
 				CreateBalloon (new Vector3 (cameraTransform.position.x + 0.0f, cameraTransform.position.y, cameraTransform.position.z + 9.0f) + RandomPosition);
 			}
 			GameDirector.GetComponent<GameDirector>().DescriptionClicked();
 		# else
 			//ランダム範囲(暫定。実機見ながら調整する)
 			for (int i = 0; i < balloonCount; i++) {
-				float RandomPositionX = Random.Range(-30,30)/10.0f;
-				float RandomPositionY = Random.Range(-10,10)/10.0f;
-				float RandomPositionZ = Random.Range(-30,30)/10.0f;
-
-				Vector3 RandomPosition = new Vector3(RandomPositionX, RandomPositionY, RandomPositionZ);
+				Vector3 RandomPosition = GenerateRandomPosition(30, 10, 30, 5);
 				CreateBalloon(cameraTransform.position + RandomPosition);
 			}
 			GameDirector.GetComponent<GameDirector>().DescriptionClicked();
 		#endif
 	}
 
+	// 中央マージンつきランダム座標生成関数（正負の振り分けは完全ランダム）
+	private Vector3 GenerateRandomPosition(int xDist, int yDist, int zDist, int centerMargin = 0)
+	{
+		float x,y,z = 0f;
+
+		// X
+		if (Random.Range(-1, 1) > 0)
+		{
+			x = Random.Range(-1*xDist, -1*centerMargin)/10.0f;
+		}
+		else
+		{
+			x = Random.Range(centerMargin, xDist)/10.0f;
+		}
+
+		// Y
+		if (Random.Range(-1, 1) > 0)
+		{
+			y = Random.Range(-1*yDist, -1*centerMargin)/10.0f;
+		}
+		else
+		{
+			y = Random.Range(centerMargin, yDist)/10.0f;
+		}
+
+		// Z
+		if (Random.Range(-1, 1) > 0)
+		{
+			z = Random.Range(-1*zDist, -1*centerMargin)/10.0f;
+		}
+		else
+		{
+			z = Random.Range(centerMargin, zDist)/10.0f;
+		}
+
+		return new Vector3(x,y,z);
+	}
 	public void PresetArrangement(List<Vector3> positions)
 	{
 		cameraTransform = mainCamera.transform;
